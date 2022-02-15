fa96824d43699243d3f35750a5f515ebe05a95e7
Rafał Korman <rafalkor5@gmail.com>
2022-02-15 17:10:39 +0100

    Add new Order and Order Test

---
+++ src/main/java/pl/rafal/testing/Order.java
@@ -0,0 +1,29 @@
+package pl.rafal.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<Meal> meals = new ArrayList<>();
+
+    public void addMealToOrder(Meal meal){
+        this.meals.add(meal);
+    }
+
+    public void removeMealFromOrder(Meal meal){
+        this.meals.remove(meal);
+    }
+    public List<Meal> getMeals() {
+        return meals;
+    }
+    public void cancel(){
+        this.meals.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "meals=" + meals +
+                '}';
+    }
+}
---
+++ src/test/java/pl/rafal/testing/OrderTest.java
@@ -0,0 +1,33 @@
+package pl.rafal.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void testAssertArrayEquals(){
+        //given
+        int[] ints1 = {1,2,3};
+        int[] ints2 = {1,2,3};
+        //then
+        assertArrayEquals(ints1,ints2);
+
+    }
+
+    @Test
+    void mealListSchouldBeEmptyAfterCreationOfOrder(){
+        //given
+        Order order = new Order();
+        //then
+        assertThat(order.getMeals(), empty());
+        assertThat(order.getMeals().size(), equalTo(0));
+        assertThat(order.getMeals(), hasSize(0));
+        assertThat(order.getMeals(), emptyCollectionOf(Meal.class));
+
+
+    }
+}
